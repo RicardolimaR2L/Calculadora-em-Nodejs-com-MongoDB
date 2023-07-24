cdc4f3c7cc00579f88087ffa736fe6e1e9dbdd69
Ricardo <ricardolimadelemos.trab@gmail.com>
2023-07-24 12:38:59 -0300

    rotas Get e Post

--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,8 @@
 const express = require('express') // importação do express
 const routes = express.Router()
 
-
+//Rotas do serviço
+app.get('/calculadora', controllerCalculadora.index)
+app.post('/calculadora', controllerCalculadora.store)
 
 module.exports = routes //exportação das rotas
